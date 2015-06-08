e5fcf208f01c82bded50d044a47f33039d9c6350
martin2250 <martin.225@web.de>
2015-06-08 22:15:25 +0200

    Added PC Software, design files

---
+++ PC Software/AVRLoggerReceiver/Form1.Designer.cs
@@ -0,0 +1,326 @@
+﻿namespace AVRLoggerReceiver
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+			System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+			System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+			System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+			System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+			this.groupBox2 = new System.Windows.Forms.GroupBox();
+			this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+			this.groupBox1 = new System.Windows.Forms.GroupBox();
+			this.comboBoxPort = new System.Windows.Forms.ComboBox();
+			this.label2 = new System.Windows.Forms.Label();
+			this.buttonOpen = new System.Windows.Forms.Button();
+			this.buttonClose = new System.Windows.Forms.Button();
+			this.groupBox3 = new System.Windows.Forms.GroupBox();
+			this.progressBar1 = new System.Windows.Forms.ProgressBar();
+			this.groupBox4 = new System.Windows.Forms.GroupBox();
+			this.numericUpDownGraphSamples = new System.Windows.Forms.NumericUpDown();
+			this.label1 = new System.Windows.Forms.Label();
+			this.buttonSave = new System.Windows.Forms.Button();
+			this.serialPort = new System.IO.Ports.SerialPort(this.components);
+			this.timer1 = new System.Windows.Forms.Timer(this.components);
+			this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+			this.groupBox2.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+			this.groupBox1.SuspendLayout();
+			this.groupBox3.SuspendLayout();
+			this.groupBox4.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.numericUpDownGraphSamples)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// groupBox2
+			// 
+			this.groupBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.groupBox2.Controls.Add(this.chart1);
+			this.groupBox2.Location = new System.Drawing.Point(12, 56);
+			this.groupBox2.Name = "groupBox2";
+			this.groupBox2.Size = new System.Drawing.Size(746, 368);
+			this.groupBox2.TabIndex = 9;
+			this.groupBox2.TabStop = false;
+			this.groupBox2.Text = "Data Visualisation";
+			// 
+			// chart1
+			// 
+			this.chart1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			chartArea1.Area3DStyle.Inclination = 50;
+			chartArea1.Area3DStyle.LightStyle = System.Windows.Forms.DataVisualization.Charting.LightStyle.Realistic;
+			chartArea1.AxisX.LineColor = System.Drawing.Color.Transparent;
+			chartArea1.AxisX2.LineColor = System.Drawing.Color.Transparent;
+			chartArea1.AxisY.IsStartedFromZero = false;
+			chartArea1.AxisY.LineColor = System.Drawing.Color.Transparent;
+			chartArea1.AxisY2.Enabled = System.Windows.Forms.DataVisualization.Charting.AxisEnabled.True;
+			chartArea1.AxisY2.IsStartedFromZero = false;
+			chartArea1.AxisY2.LineColor = System.Drawing.Color.Transparent;
+			chartArea1.BorderColor = System.Drawing.Color.Transparent;
+			chartArea1.Name = "ChartArea1";
+			chartArea1.Position.Auto = false;
+			chartArea1.Position.Height = 83.98245F;
+			chartArea1.Position.Width = 94F;
+			chartArea1.Position.X = 3F;
+			chartArea1.Position.Y = 3F;
+			this.chart1.ChartAreas.Add(chartArea1);
+			legend1.Alignment = System.Drawing.StringAlignment.Center;
+			legend1.Docking = System.Windows.Forms.DataVisualization.Charting.Docking.Bottom;
+			legend1.Name = "Legend1";
+			this.chart1.Legends.Add(legend1);
+			this.chart1.Location = new System.Drawing.Point(9, 19);
+			this.chart1.Name = "chart1";
+			series1.BorderWidth = 2;
+			series1.ChartArea = "ChartArea1";
+			series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+			series1.Color = System.Drawing.Color.Red;
+			series1.Legend = "Legend1";
+			series1.Name = "Temperature";
+			series1.XValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.DateTime;
+			series1.YValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.Double;
+			series2.BorderWidth = 2;
+			series2.ChartArea = "ChartArea1";
+			series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+			series2.Color = System.Drawing.Color.Blue;
+			series2.Legend = "Legend1";
+			series2.Name = "Humidity";
+			series2.XValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.DateTime;
+			series2.YAxisType = System.Windows.Forms.DataVisualization.Charting.AxisType.Secondary;
+			series2.YValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.Double;
+			series3.BorderWidth = 2;
+			series3.ChartArea = "ChartArea1";
+			series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+			series3.Color = System.Drawing.Color.Lime;
+			series3.Legend = "Legend1";
+			series3.Name = "Battery";
+			series3.XValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.DateTime;
+			series3.YAxisType = System.Windows.Forms.DataVisualization.Charting.AxisType.Secondary;
+			series3.YValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.Double;
+			this.chart1.Series.Add(series1);
+			this.chart1.Series.Add(series2);
+			this.chart1.Series.Add(series3);
+			this.chart1.Size = new System.Drawing.Size(731, 343);
+			this.chart1.TabIndex = 0;
+			this.chart1.Text = "chart1";
+			// 
+			// groupBox1
+			// 
+			this.groupBox1.Controls.Add(this.comboBoxPort);
+			this.groupBox1.Controls.Add(this.label2);
+			this.groupBox1.Controls.Add(this.buttonOpen);
+			this.groupBox1.Controls.Add(this.buttonClose);
+			this.groupBox1.Location = new System.Drawing.Point(12, 12);
+			this.groupBox1.Name = "groupBox1";
+			this.groupBox1.Size = new System.Drawing.Size(336, 38);
+			this.groupBox1.TabIndex = 10;
+			this.groupBox1.TabStop = false;
+			this.groupBox1.Text = "Data Source";
+			// 
+			// comboBoxPort
+			// 
+			this.comboBoxPort.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+			this.comboBoxPort.FormattingEnabled = true;
+			this.comboBoxPort.Location = new System.Drawing.Point(41, 13);
+			this.comboBoxPort.Name = "comboBoxPort";
+			this.comboBoxPort.Size = new System.Drawing.Size(121, 21);
+			this.comboBoxPort.TabIndex = 6;
+			this.comboBoxPort.DropDown += new System.EventHandler(this.comboBoxPort_DropDown);
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(6, 16);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(29, 13);
+			this.label2.TabIndex = 5;
+			this.label2.Text = "Port:";
+			// 
+			// buttonOpen
+			// 
+			this.buttonOpen.Location = new System.Drawing.Point(168, 11);
+			this.buttonOpen.Name = "buttonOpen";
+			this.buttonOpen.Size = new System.Drawing.Size(75, 23);
+			this.buttonOpen.TabIndex = 3;
+			this.buttonOpen.Text = "Open";
+			this.buttonOpen.UseVisualStyleBackColor = true;
+			this.buttonOpen.Click += new System.EventHandler(this.buttonOpen_Click);
+			// 
+			// buttonClose
+			// 
+			this.buttonClose.Enabled = false;
+			this.buttonClose.Location = new System.Drawing.Point(249, 11);
+			this.buttonClose.Name = "buttonClose";
+			this.buttonClose.Size = new System.Drawing.Size(75, 23);
+			this.buttonClose.TabIndex = 4;
+			this.buttonClose.Text = "Close";
+			this.buttonClose.UseVisualStyleBackColor = true;
+			this.buttonClose.Click += new System.EventHandler(this.buttonClose_Click);
+			// 
+			// groupBox3
+			// 
+			this.groupBox3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.groupBox3.Controls.Add(this.progressBar1);
+			this.groupBox3.Location = new System.Drawing.Point(354, 12);
+			this.groupBox3.Name = "groupBox3";
+			this.groupBox3.Size = new System.Drawing.Size(404, 38);
+			this.groupBox3.TabIndex = 11;
+			this.groupBox3.TabStop = false;
+			this.groupBox3.Text = "Progress";
+			// 
+			// progressBar1
+			// 
+			this.progressBar1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.progressBar1.Location = new System.Drawing.Point(6, 15);
+			this.progressBar1.Name = "progressBar1";
+			this.progressBar1.Size = new System.Drawing.Size(392, 19);
+			this.progressBar1.TabIndex = 0;
+			// 
+			// groupBox4
+			// 
+			this.groupBox4.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.groupBox4.Controls.Add(this.numericUpDownGraphSamples);
+			this.groupBox4.Controls.Add(this.label1);
+			this.groupBox4.Location = new System.Drawing.Point(12, 430);
+			this.groupBox4.Name = "groupBox4";
+			this.groupBox4.Size = new System.Drawing.Size(640, 45);
+			this.groupBox4.TabIndex = 12;
+			this.groupBox4.TabStop = false;
+			this.groupBox4.Text = "Settings";
+			// 
+			// numericUpDownGraphSamples
+			// 
+			this.numericUpDownGraphSamples.Location = new System.Drawing.Point(91, 14);
+			this.numericUpDownGraphSamples.Maximum = new decimal(new int[] {
+            1024,
+            0,
+            0,
+            0});
+			this.numericUpDownGraphSamples.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+			this.numericUpDownGraphSamples.Name = "numericUpDownGraphSamples";
+			this.numericUpDownGraphSamples.Size = new System.Drawing.Size(43, 20);
+			this.numericUpDownGraphSamples.TabIndex = 1;
+			this.numericUpDownGraphSamples.Value = new decimal(new int[] {
+            128,
+            0,
+            0,
+            0});
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(6, 16);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(79, 13);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "DisplayInterval:";
+			// 
+			// buttonSave
+			// 
+			this.buttonSave.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+			this.buttonSave.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.buttonSave.Location = new System.Drawing.Point(658, 430);
+			this.buttonSave.Name = "buttonSave";
+			this.buttonSave.Size = new System.Drawing.Size(100, 45);
+			this.buttonSave.TabIndex = 13;
+			this.buttonSave.Text = "Save";
+			this.buttonSave.UseVisualStyleBackColor = true;
+			this.buttonSave.Click += new System.EventHandler(this.buttonSave_Click);
+			// 
+			// serialPort
+			// 
+			this.serialPort.BaudRate = 250000;
+			// 
+			// timer1
+			// 
+			this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+			// 
+			// saveFileDialog1
+			// 
+			this.saveFileDialog1.DefaultExt = "html";
+			this.saveFileDialog1.FileName = "Klimadaten.html";
+			this.saveFileDialog1.Filter = "Html Files|*.html|Csv Files|*.csv";
+			this.saveFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.saveFileDialog1_FileOk);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(770, 487);
+			this.Controls.Add(this.buttonSave);
+			this.Controls.Add(this.groupBox4);
+			this.Controls.Add(this.groupBox3);
+			this.Controls.Add(this.groupBox1);
+			this.Controls.Add(this.groupBox2);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+			this.groupBox2.ResumeLayout(false);
+			((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+			this.groupBox1.ResumeLayout(false);
+			this.groupBox1.PerformLayout();
+			this.groupBox3.ResumeLayout(false);
+			this.groupBox4.ResumeLayout(false);
+			this.groupBox4.PerformLayout();
+			((System.ComponentModel.ISupportInitialize)(this.numericUpDownGraphSamples)).EndInit();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.GroupBox groupBox2;
+		private System.Windows.Forms.GroupBox groupBox1;
+		private System.Windows.Forms.ComboBox comboBoxPort;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Button buttonOpen;
+		private System.Windows.Forms.Button buttonClose;
+		private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+		private System.Windows.Forms.GroupBox groupBox3;
+		private System.Windows.Forms.ProgressBar progressBar1;
+		private System.Windows.Forms.GroupBox groupBox4;
+		private System.Windows.Forms.NumericUpDown numericUpDownGraphSamples;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Button buttonSave;
+		private System.IO.Ports.SerialPort serialPort;
+		private System.Windows.Forms.Timer timer1;
+		private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+
+	}
+}
+
---
+++ PC Software/AVRLoggerReceiver/Form1.cs
@@ -0,0 +1,280 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.Windows.Forms.DataVisualization.Charting;
+using System.IO;
+
+namespace AVRLoggerReceiver
+{
+	public partial class Form1 : Form
+	{
+
+		static string PointPair(DateTime t, double value)
+		{
+			return string.Format("[Date.UTC({0}, {1}, {2}, {3}, {4}), {5}]", t.Year, t.Month - 1, t.Day, t.Hour, t.Minute, value.ToString().Replace(',', '.'));
+		}
+
+		List<DataPoint> Measurements = new List<DataPoint>();
+
+		Series TemperatureS, HumidityS, BatteryS;
+
+		receiverSettings Settings = receiverSettings.Default;
+
+		public Form1()
+		{
+			InitializeComponent();
+
+			try { numericUpDownGraphSamples.Value = Settings.GraphPoints; }
+			catch { }
+
+			List<string> ports = new List<string>(SerialPort.GetPortNames());
+
+			comboBoxPort.Items.AddRange(ports.ToArray());
+
+			if (ports.IndexOf(Settings.Port) != -1)
+				comboBoxPort.SelectedIndex = ports.IndexOf(Settings.Port);
+
+			TemperatureS = chart1.Series[0];
+			HumidityS = chart1.Series[1];
+			BatteryS = chart1.Series[2];
+
+		}
+
+		private void comboBoxPort_DropDown(object sender, EventArgs e)
+		{
+			comboBoxPort.Items.Clear();
+			comboBoxPort.Items.AddRange(SerialPort.GetPortNames());
+		}
+
+		private void buttonOpen_Click(object sender, EventArgs e)
+		{
+			serialPort.PortName = (string)comboBoxPort.SelectedItem;
+
+			try
+			{
+				serialPort.Open();
+
+				TemperatureS.Points.Clear();
+				HumidityS.Points.Clear();
+				BatteryS.Points.Clear();
+				Measurements.Clear();
+				progressBar1.Value = 0;
+
+				PageBuffer = new byte[128];
+				BufferPointer = 0;
+				TotalPages = -1;
+				PageCounter = 0;
+
+				timer1.Start();
+
+				buttonOpen.Enabled = false;
+				buttonClose.Enabled = true;
+				numericUpDownGraphSamples.Enabled = false;
+
+				samplepointer = (int)numericUpDownGraphSamples.Value - 1;
+			}
+			catch
+			{
+				MessageBox.Show("Could not open Port");
+			}
+		}
+
+		private void buttonSave_Click(object sender, EventArgs e)
+		{
+			saveFileDialog1.ShowDialog();
+		}
+
+		private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+		{
+			Settings.GraphPoints = (int)numericUpDownGraphSamples.Value;
+			Settings.Port = (string)comboBoxPort.SelectedItem;
+			Settings.Save();
+		}
+
+		byte[] PageBuffer = new byte[128];
+		int BufferPointer = 0;
+
+		int TotalPages = -1;
+		int PageCounter = 0;
+
+		int samplepointer = 0;
+
+		private void timer1_Tick(object sender, EventArgs e)
+		{
+			while (serialPort.BytesToRead > 0)
+			{
+				if (TotalPages == -1)
+				{
+
+					int i = serialPort.ReadByte();
+					if (i == 0)
+						continue;
+					if (i == 255)
+					{
+						while (serialPort.BytesToRead < 2) ;
+						TotalPages = serialPort.ReadByte() << 8;
+						TotalPages |= serialPort.ReadByte();
+
+
+
+						progressBar1.Maximum = TotalPages;
+					}
+
+				}
+				else
+				{
+					BufferPointer += serialPort.Read(PageBuffer, BufferPointer, 128 - BufferPointer);
+				}
+
+				if (BufferPointer == 128)
+				{
+					BufferPointer = 0;
+
+					progressBar1.Value++;
+					PageCounter++;
+
+					int Samples = PageBuffer[0];
+
+					int Interval = PageBuffer[1] & 0x3F;
+
+					int batraw = PageBuffer[1] >> 6;
+					batraw |= (PageBuffer[2] & 0xF) << 2;
+
+					float? Battery = batraw / 10.0F;
+
+					int Month = PageBuffer[2] >> 4;
+
+					int Year = PageBuffer[3] + 2000;
+
+					int Day = PageBuffer[4];
+					int Hour = PageBuffer[5];
+					int Minute = PageBuffer[6];
+					int Second = PageBuffer[7];
+
+					DateTime Time;
+
+					Time = new DateTime(Year, Month, Day, Hour, Minute, Second);
+
+					for (int i = 0; i < Samples; i++)
+					{
+						float Temperature = ((int)PageBuffer[8 + i * 3]) << 4 | PageBuffer[9 + i * 3] >> 4;
+						Temperature = (Temperature * 165F / 4096F) - 40;
+
+						float Humididy = ((int)PageBuffer[9 + i * 3] & 0xF) << 8 | PageBuffer[10 + i * 3];
+						Humididy = Humididy * 100 / 4096;
+
+						Measurements.Add(new DataPoint { BatteryLevel = Battery, Temperature = Temperature, Humidity = Humididy, Time = Time });
+
+						if (++samplepointer == (int)numericUpDownGraphSamples.Value)
+						{
+							TemperatureS.Points.AddXY(Time, Temperature);
+							HumidityS.Points.AddXY(Time, Humididy);
+							samplepointer = 0;
+						}
+
+						if (Battery.HasValue)
+							BatteryS.Points.AddXY(Time, Battery);
+
+						Battery = null;
+
+						Time = Time.AddMinutes(Interval);
+					}
+
+				}
+
+			}
+
+			if (PageCounter == TotalPages)
+				buttonClose_Click(null, null);
+		}
+
+		private void buttonClose_Click(object sender, EventArgs e)
+		{
+			timer1.Stop();
+			serialPort.Close();
+
+			buttonOpen.Enabled = true;
+			buttonClose.Enabled = false;
+
+			numericUpDownGraphSamples.Enabled = true;
+		}
+
+		private void saveFileDialog1_FileOk(object sender, CancelEventArgs e)
+		{
+			StreamWriter w = new StreamWriter(saveFileDialog1.FileName);
+
+			switch (saveFileDialog1.FilterIndex)
+			{
+				case 1:
+					w.Write(Properties.Resources.Graph_P1);
+
+					foreach (DataPoint p in Measurements)
+					{
+						w.Write(PointPair(p.Time, Math.Round(p.Temperature, 2)));
+						if (!p.Equals(Measurements.Last()))
+							w.Write(',');
+					}
+
+					w.Write(Properties.Resources.Graph_P2);
+
+					foreach (DataPoint p in Measurements)
+					{
+						w.Write(PointPair(p.Time, Math.Round(p.Humidity, 2)));
+						if (!p.Equals(Measurements.Last()))
+							w.Write(',');
+					}
+
+					w.Write(Properties.Resources.Graph_P3);
+
+					foreach (DataPoint p in Measurements.Where(p => p.BatteryLevel.HasValue))
+					{
+						w.Write(PointPair(p.Time, Math.Round((float)p.BatteryLevel, 1)));
+						w.Write(',');
+					}
+
+					w.Write(PointPair(Measurements.Last().Time, Math.Round((float)Measurements.Where(p => p.BatteryLevel.HasValue).Last().BatteryLevel, 1)));
+
+					w.Write(Properties.Resources.Graph_P4);
+					w.Write(Properties.Resources.Graph_P5);
+					break;
+				case 2:
+					w.Write("Time;Temperature;Humidity;Battery");
+					foreach (DataPoint p in Measurements)
+					{
+						w.WriteLine();
+						w.Write(p.Time);
+						w.Write(';');
+						w.Write(Math.Round(p.Temperature, 2));
+						w.Write(';');
+						w.Write(Math.Round(p.Humidity, 2));
+						w.Write(';');
+						if(p.BatteryLevel.HasValue)
+							w.Write(p.BatteryLevel);
+					}
+					break;
+				default:
+					MessageBox.Show("Wrong file type selected " + saveFileDialog1.FilterIndex.ToString());
+					break;
+			}
+			w.Close();
+		}
+	}
+
+	struct DataPoint
+	{
+		public float Temperature, Humidity;
+		public float? BatteryLevel;
+		public DateTime Time;
+	}
+
+
+
+
+}
