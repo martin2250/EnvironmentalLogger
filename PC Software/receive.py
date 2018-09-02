e01382fd917c840151b21cee035c98c5e30f55f5
Martin Pittermann <martin.225@web.de>
2018-09-02 11:04:33 +0200

    Create receive.py

---
+++ PC Software/receive.py
@@ -0,0 +1,108 @@
+#!/usr/bin/python
+import serial
+import argparse
+import sys
+import struct
+import datetime
+
+parser = argparse.ArgumentParser(description='receives the data from a climate logger via serial (https://github.com/martin2250/EnvironmentalLogger)', epilog='to import the data into influxDB, use this command: "influx -host <host> -import -path=<fout>"')
+parser.add_argument('port', help='which serial port to use')
+parser.add_argument('fout', help='output file name, \'-\' = stdio')
+parser.add_argument('-f', '--format', help='output file format', choices=['influx', 'tsv'], default='tsv')
+parser.add_argument('--measurement', help='measurement name and tags (only for influx output format)', default='test')
+parser.add_argument('--database', help='database (only for influx output format)', default='<DB>')
+args = parser.parse_args()
+
+
+def nofunc(*args):
+	pass
+
+def start_influx(file):
+	global args
+	file.write(f'# DML\n# CONTEXT-DATABASE: {args.database}\n'
+
+def write_influx(file, date, temperature, humidity, battery = None):
+	global args
+	file.write(args.measurement)
+	file.write(f' temp={temperature:0.2f},hum={humidity:0.2f}')
+	if battery is not None:
+		file.write(f',bat={battery:0.1f}')
+	file.write(f' {date.timestamp() * 1e9:0.0f}\n')
+
+def start_tsv(file):
+	file.write('# time\ttemperature (celsius)\thumidity (rH)\tbattery (volt)\n')
+
+def write_tsv(file, date, temperature, humidity, battery = None):
+	file.write(f'{date}\t{temperature:0.2f}\t{humidity:0.2f}')
+	if battery is not None:
+		file.write(f'\t{battery:0.1f}')
+	file.write('\n')
+
+file_start = nofunc
+file_write = nofunc
+file_end = nofunc
+
+if args.format == 'influx':
+	file_start = start_influx
+	file_write = write_influx
+elif args.format == 'tsv':
+	file_start = start_tsv
+	file_write = write_tsv
+
+
+with serial.Serial(args.port, 250000) as port, sys.stdout if args.fout == '-' else open(args.fout, 'w') as fout:
+	port.reset_input_buffer()
+	print(f'opened {port.name}, waiting for start sequence')
+
+	while True:
+		x = port.read()
+		if x[0] == 0xFF:
+			break
+		elif x[0] != 0x00:
+			print('invalid start sequence, aborting')
+			exit(1)
+
+	pagecount, = struct.unpack('>H', port.read(2))
+
+	print(f'received start sequence, {pagecount} pages expected')
+
+	port.timeout = 1.0
+	pagecounter = 0
+
+	file_start(fout)
+
+	while pagecounter < pagecount:
+		page = port.read(128)
+
+		if len(page) != 128:
+			print(f'\nwrong number of bytes received: {len(page)}')
+			exit(1)
+
+		pagecounter += 1
+		print(f'[{pagecounter}/{pagecount}]', end='')
+
+		samples = page[0]
+		interval_minutes = page[1] & 0x3F
+		battery_raw = (page[1] >> 6) | ((page[2] & 0x0F) << 2)
+		battery = battery_raw / 10
+
+		month = page[2] >> 4
+		year = page[3] + 2000
+		day = page[4]
+		hour = page[5]
+		minute = page[6]
+		second = page[7]
+
+		date = datetime.datetime(year, month, day, hour, minute, second)
+		print(' ', date, end = '\r')
+
+		for i in range(samples):
+			temperature_raw = (page[8 + i * 3] << 4) | (page[9 + i * 3] >> 4)
+			humidity_raw = ((page[9 + i * 3] & 0x0F) << 8) | page[10 + i * 3]
+
+			temperature = temperature_raw * 165/4096 - 40
+			humidity = humidity_raw * 100 / 4096
+
+			file_write(fout, date + datetime.timedelta(minutes=i*interval_minutes), temperature, humidity, battery if i == 0 else None)
+	file_end(fout)
+print('\ndone')
